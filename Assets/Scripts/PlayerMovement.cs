382843f0f028ec04f82845e631c304ad7836e3cc
Kendra Gamett <kgamett@student.neumont.edu>
2024-04-01 11:36:35 -0600

    Level 1 Designed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 3;
 
+    Vector2 vel = Vector2.zero;
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    bool faceRight = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = rb.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        float direction = Input.GetAxis("Vertical");
-        float movement = direction * speed * Time.deltaTime;
+        vel.x = Input.GetAxis("Horizontal") * speed;
+        vel.y = Input.GetAxis("Vertical") * speed;
 
+        if (vel.x < 0 && !faceRight) FlipSprite();
+        if (vel.x > 0 && faceRight) FlipSprite();
 
+        rb.velocity = vel;
+    }
+
+    private void FlipSprite()
+    {
+        faceRight = !faceRight;
+        spriteRenderer.flipX = !faceRight;
     }
 }
